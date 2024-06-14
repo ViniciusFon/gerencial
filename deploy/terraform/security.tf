522bf14e6f65bc9cc430c522ed0b5d6f5d648a70
viniciusFon <vinicius.fontana@gmail.com>
2024-06-13 23:05:25 -0300

    Ajustes para teste no docker

---
+++ deploy/terraform/security.tf
@@ -0,0 +1,61 @@
+
+resource "aws_security_group" "loja_sg" {
+  name        = "loja_sg"
+  description = "loja security group"
+  vpc_id      = aws_vpc.loja_vpc_1.id
+
+  tags = {
+    "Name" = "loja_sg"
+  }
+}
+
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.loja_sg.id
+}
+
+resource "aws_security_group_rule" "public_in_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.loja_sg.id
+}
+
+resource "aws_key_pair" "loja_key" {
+  key_name   = "loja_key"
+  public_key = file("~/.ssh/loja_key.pub")
+}
+
+resource "aws_security_group_rule" "public_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.loja_sg.id
+}
+
+resource "aws_security_group_rule" "public_in_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.loja_sg.id
+}
+
+resource "aws_security_group_rule" "public_in_mongodb" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.loja_sg.id
+}
